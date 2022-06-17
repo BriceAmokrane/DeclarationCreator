89159aca2b02e6eb9186abd547336059ee6f5b42
BriceAmokrane <briceamokranedev@gmail.com>
2022-06-17 07:28:52 +0200

    Ajout de routes

--- api/src/main/java/com/projetmipih/api/controller/DeclarationController.java
+++ api/src/main/java/com/projetmipih/api/controller/DeclarationController.java
@@ -1,7 +1,13 @@
 package com.projetmipih.api.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetmipih.api.model.Declaration;
@@ -19,5 +25,19 @@ public class DeclarationController {
 		return declarationService.getAllDeclarations();
 	}
 	
+	@PostMapping("/createDeclaration")
+	public Declaration createDeclaration(@RequestBody Declaration declaration) {
+		return declarationService.saveDeclaration(declaration);
+	}
+	
+	@GetMapping("/declaration/{id}")
+	public Optional<Declaration> getDeclaration(@PathVariable("id") Long id) {
+		return declarationService.getDeclaration(id);
+	}
+	
+	@DeleteMapping("/declarationdelete/{id}")
+	public void deleteDeclaration(@PathVariable("id") Long id) {
+		declarationService.deleteDeclaration(id);
+	}
 	
 }
