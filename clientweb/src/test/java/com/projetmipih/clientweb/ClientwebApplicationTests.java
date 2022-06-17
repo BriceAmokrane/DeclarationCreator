87ffa4b285f36fee482c7391646ff020cb2cbdac
BriceAmokrane <briceamokranedev@gmail.com>
2022-06-17 06:30:03 +0200

    Spring Initializr Client web

---
+++ clientweb/src/test/java/com/projetmipih/clientweb/ClientwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetmipih.clientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
